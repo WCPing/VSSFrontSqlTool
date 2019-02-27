dde3597202091f0c84a2a85a8e7776d0d96e2203
wuchangping <wuchangping@hd123.com>
2019-02-27 20:06:02 +0800

    新文件

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="scroll">
-        123
+        x: {{posX}}
+        <div class="container" @mousemove="mousemove">
+
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            posX: 0
+        }
+    },
+    methods: {
+        mousemove(e) {
+            // 获取x 坐标
+            this.posX = e.offsetX 
+        }
+    },   
 }
 </script>
 <style lang="scss" scoped>
-
+.scroll{
+    .container{
+        width: 960px;
+        height: 540px;
+        background-color: #cccccc;
+    }
+}
 </style>
