be297f615024edd73d3cf12e586c88954cac1b02
WCPing <w84814077@163.com>
2019-02-27 22:26:31 +0800

    complete

--- src/components/Scroll.vue
+++ src/components/Scroll.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="scroll">
-        x: {{posX}}
         <div class="container" @mousemove="mousemove">
-
+            <div class="base"></div>
+            <div class="left" ref="left">
+                <img src="../../static/image/kda-karsa.jpg" alt="">
+            </div>
         </div>
     </div>
 </template>
@@ -18,7 +20,12 @@ export default {
             // 获取x 坐标
             this.posX = e.offsetX 
         }
-    },   
+    },
+    watch: {
+        posX(curX) {
+            this.$refs.left.style.width = `${curX}px`
+        }   
+    }
 }
 </script>
 <style lang="scss" scoped>
@@ -27,6 +34,29 @@ export default {
         width: 960px;
         height: 540px;
         background-color: #cccccc;
+        position: relative;
+        cursor: ew-resize;
+        .base{
+            position: absolute;
+            width: 960px;
+            height: 540px;
+            top: 0;
+            left: 0;
+            background: url('../../static/image/kda-karsa-golden.jpg') no-repeat;
+            background-size: 100%;
+        }
+        .left{
+            position: absolute;
+            width: 480px;
+            height: 540px;
+            overflow: hidden;
+            top: 0;
+            left: 0;
+            img{
+              width: 960px;
+              height: 540px;  
+            }
+        }
     }
 }
 </style>
